7f61dbca5b1d54224898ac7d3ac62f066c6c4b0b
Kevin Manoti <kevinnoty21@gmail.com>
2026-02-12 17:16:57 +0300

    updated req packages

--- ethereum_dbt/models/staging/stg_all_crypto.sql
+++ ethereum_dbt/models/staging/stg_all_crypto.sql
@@ -1,24 +1,27 @@
-{% set coins = [
-    'bitcoin',
-    'ethereum',
-    'solana',
-    'dogecoin',
-    'cardano',
-    'xrp'
-] %}
--- Add the rest of your coins here gradually
+{% set relations = dbt_utils.get_relations_by_pattern(
+    schema=target.schema,
+    pattern='%'
+) %}
 
-{% for coin in coins %}
+{% set seed_tables = [] %}
+
+{% for relation in relations %}
+    {% if relation.identifier not in ['stg_all_crypto'] %}
+        {% do seed_tables.append(relation) %}
+    {% endif %}
+{% endfor %}
+
+{% for table in seed_tables %}
 
 select
-    '{{ coin }}' as asset,
+    '{{ table.identifier }}' as asset,
     cast(Date as date) as date,
     cast(Open as double) as open_price,
     cast(High as double) as high_price,
     cast(Low as double) as low_price,
     cast(Close as double) as close_price,
     cast(Volume as double) as volume
-from {{ ref(coin) }}
+from {{ table }}
 
 {% if not loop.last %} union all {% endif %}
 
