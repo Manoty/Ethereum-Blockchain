70ac094ce1beb3f83992faa7590dabe1bb52482d
Kevin Manoti <kevinnoty21@gmail.com>
2026-02-14 00:18:14 +0300

    updated int model

--- ethereum_dbt/models/intermediate/int_crypto_features.sql
+++ ethereum_dbt/models/intermediate/int_crypto_features.sql
@@ -1,34 +1,32 @@
--- models/intermediate/int_crypto_features.sql
+with base as (
+
+    select
+        asset,
+        date,
+        open_price,
+        close_price,
+        high_price,
+        low_price,
+        volume,
+
+        -- daily return
+        case
+            when open_price > 0
+            then (close_price - open_price) / open_price
+            else null
+        end as daily_return,
+
+        -- log return
+        case
+            when open_price > 0
+             and close_price > 0
+            then ln(close_price / open_price)
+            else null
+        end as log_return
+
+    from {{ ref('stg_all_crypto') }}
+
+)
 
 select *
-from (
-    with base as (
-        select
-            asset,
-            date,
-            cast(open_price as double) as open_price,
-            cast(close_price as double) as close_price,
-            cast(high as double) as high,
-            cast(low as double) as low,
-            cast(volume as double) as volume,
-
-            -- daily return
-            case
-                when open_price is not null and cast(open_price as double) != 0
-                then (cast(close_price as double) - cast(open_price as double)) / cast(open_price as double)
-                else null
-            end as daily_return,
-
-            -- log return
-            case
-                when open_price is not null and cast(open_price as double) > 0
-                     and close_price is not null and cast(close_price as double) > 0
-                then ln(cast(close_price as double) / cast(open_price as double))
-                else null
-            end as log_return
-
-        from {{ ref('stg_all_crypto') }}
-    )
-    select *
-    from base
-) as t
+from base
