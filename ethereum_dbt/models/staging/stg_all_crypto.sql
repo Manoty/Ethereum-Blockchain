c9740252b0e10648996233a91eee828907fe9037
Kevin Manoti <kevinnoty21@gmail.com>
2026-02-13 14:34:07 +0300

    updated staging model

--- ethereum_dbt/models/staging/stg_all_crypto.sql
+++ ethereum_dbt/models/staging/stg_all_crypto.sql
@@ -1,36 +1,204 @@
-{{ config(materialized='view') }}
+-- models/staging/stg_all_crypto.sql
+-- Staging model: combines all crypto CSVs into one table
 
--- Hardcode schema to main
-{% set relations = dbt_utils.get_relations_by_pattern(
-    schema_pattern='main',
-    table_pattern='%'
-) %}
+with all_data as (
 
--- Filter out THIS model + only include tables that exist (seeds)
-{% set seed_tables = [] %}
-{% for relation in relations %}
-    {% if relation.type == 'table' and relation.identifier != 'stg_all_crypto' %}
-        {% do seed_tables.append(relation) %}
-    {% endif %}
-{% endfor %}
+    select 'aave' as asset, date, open as open_price, high, low, close as close_price, volume
+    from read_csv_auto('seeds/aave.csv')
 
--- Fail fast if nothing found
-{% if seed_tables | length == 0 %}
-    {{ exceptions.raise_compiler_error("No seed tables found in main schema. Run dbt seed first.") }}
-{% endif %}
+    union all
+    select 'algorand', date, open, high, low, close, volume
+    from read_csv_auto('seeds/algorand.csv')
 
--- Union all crypto tables
-{% for table in seed_tables %}
-    select
-        '{{ table.identifier }}' as asset,
-        cast(Date as date) as date,
-        cast(Open as double) as open_price,
-        cast(High as double) as high_price,
-        cast(Low as double) as low_price,
-        cast(Close as double) as close_price,
-        cast(Volume as double) as volume
-    from {{ table }}
-    {% if not loop.last %}
-        union all
-    {% endif %}
-{% endfor %}
+    union all
+    select 'aptos', date, open, high, low, close, volume
+    from read_csv_auto('seeds/aptos.csv')
+
+    union all
+    select 'arbitrum', date, open, high, low, close, volume
+    from read_csv_auto('seeds/arbitrum.csv')
+
+    union all
+    select 'avalanche', date, open, high, low, close, volume
+    from read_csv_auto('seeds/avalanche.csv')
+
+    union all
+    select 'axie_infinity', date, open, high, low, close, volume
+    from read_csv_auto('seeds/axie_infinity.csv')
+
+    union all
+    select 'binance_coin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/binance_coin.csv')
+
+    union all
+    select 'bitcoin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/bitcoin.csv')
+
+    union all
+    select 'bitcoin_cash', date, open, high, low, close, volume
+    from read_csv_auto('seeds/bitcoin_cash.csv')
+
+    union all
+    select 'cardano', date, open, high, low, close, volume
+    from read_csv_auto('seeds/cardano.csv')
+
+    union all
+    select 'chainlink', date, open, high, low, close, volume
+    from read_csv_auto('seeds/chainlink.csv')
+
+    union all
+    select 'cosmos', date, open, high, low, close, volume
+    from read_csv_auto('seeds/cosmos.csv')
+
+    union all
+    select 'decentraland', date, open, high, low, close, volume
+    from read_csv_auto('seeds/decentraland.csv')
+
+    union all
+    select 'dogecoin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/dogecoin.csv')
+
+    union all
+    select 'eos', date, open, high, low, close, volume
+    from read_csv_auto('seeds/eos.csv')
+
+    union all
+    select 'ethereum', date, open, high, low, close, volume
+    from read_csv_auto('seeds/ethereum.csv')
+
+    union all
+    select 'fantom', date, open, high, low, close, volume
+    from read_csv_auto('seeds/fantom.csv')
+
+    union all
+    select 'filecoin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/filecoin.csv')
+
+    union all
+    select 'flow', date, open, high, low, close, volume
+    from read_csv_auto('seeds/flow.csv')
+
+    union all
+    select 'hedera', date, open, high, low, close, volume
+    from read_csv_auto('seeds/hedera.csv')
+
+    union all
+    select 'immutable', date, open, high, low, close, volume
+    from read_csv_auto('seeds/immutable.csv')
+
+    union all
+    select 'injective', date, open, high, low, close, volume
+    from read_csv_auto('seeds/injective.csv')
+
+    union all
+    select 'internet_computer', date, open, high, low, close, volume
+    from read_csv_auto('seeds/internet_computer.csv')
+
+    union all
+    select 'kaspa', date, open, high, low, close, volume
+    from read_csv_auto('seeds/kaspa.csv')
+
+    union all
+    select 'lido', date, open, high, low, close, volume
+    from read_csv_auto('seeds/lido.csv')
+
+    union all
+    select 'litecoin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/litecoin.csv')
+
+    union all
+    select 'maker', date, open, high, low, close, volume
+    from read_csv_auto('seeds/maker.csv')
+
+    union all
+    select 'near', date, open, high, low, close, volume
+    from read_csv_auto('seeds/near.csv')
+
+    union all
+    select 'optimism', date, open, high, low, close, volume
+    from read_csv_auto('seeds/optimism.csv')
+
+    union all
+    select 'pepe', date, open, high, low, close, volume
+    from read_csv_auto('seeds/pepe.csv')
+
+    union all
+    select 'polkadot', date, open, high, low, close, volume
+    from read_csv_auto('seeds/polkadot.csv')
+
+    union all
+    select 'polygon', date, open, high, low, close, volume
+    from read_csv_auto('seeds/polygon.csv')
+
+    union all
+    select 'render', date, open, high, low, close, volume
+    from read_csv_auto('seeds/render.csv')
+
+    union all
+    select 'sandbox', date, open, high, low, close, volume
+    from read_csv_auto('seeds/sandbox.csv')
+
+    union all
+    select 'shiba_inu', date, open, high, low, close, volume
+    from read_csv_auto('seeds/shiba_inu.csv')
+
+    union all
+    select 'solana', date, open, high, low, close, volume
+    from read_csv_auto('seeds/solana.csv')
+
+    union all
+    select 'stacks', date, open, high, low, close, volume
+    from read_csv_auto('seeds/stacks.csv')
+
+    union all
+    select 'stellar', date, open, high, low, close, volume
+    from read_csv_auto('seeds/stellar.csv')
+
+    union all
+    select 'sui', date, open, high, low, close, volume
+    from read_csv_auto('seeds/sui.csv')
+
+    union all
+    select 'tether', date, open, high, low, close, volume
+    from read_csv_auto('seeds/tether.csv')
+
+    union all
+    select 'tezos', date, open, high, low, close, volume
+    from read_csv_auto('seeds/tezos.csv')
+
+    union all
+    select 'theta', date, open, high, low, close, volume
+    from read_csv_auto('seeds/theta.csv')
+
+    union all
+    select 'the_graph', date, open, high, low, close, volume
+    from read_csv_auto('seeds/the_graph.csv')
+
+    union all
+    select 'toncoin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/toncoin.csv')
+
+    union all
+    select 'tron', date, open, high, low, close, volume
+    from read_csv_auto('seeds/tron.csv')
+
+    union all
+    select 'uniswap', date, open, high, low, close, volume
+    from read_csv_auto('seeds/uniswap.csv')
+
+    union all
+    select 'usd_coin', date, open, high, low, close, volume
+    from read_csv_auto('seeds/usd_coin.csv')
+
+    union all
+    select 'vechain', date, open, high, low, close, volume
+    from read_csv_auto('seeds/vechain.csv')
+
+    union all
+    select 'xrp', date, open, high, low, close, volume
+    from read_csv_auto('seeds/xrp.csv')
+
+)
+
+select *
+from all_data
